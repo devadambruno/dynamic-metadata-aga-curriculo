71e44ab8645819f00aa5601754a19c985fd34861
Bruno Adam <dev.adambruno@gmail.com>
2025-11-09 08:07:00 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,10 +4,6 @@ export const config = {
       {
           pattern: "/perfilpublico/[^/]+",
           metaDataEndpoint: "https://api.argologerenciadoraacervos.com.br/api:tmCisItK/usercurriculo/{paramuser}/metadata"
-      },
-      {
-          pattern: "/team/profile/[^/]+",
-          metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:LjwxezTv/team/profile/{profile_id}/meta"
       }
       // Add more patterns and their metadata endpoints as needed
   ]
