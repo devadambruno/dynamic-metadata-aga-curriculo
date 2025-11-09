a88d4566356ab64c32dfed7bfbc5b1dcc7ec0159
Bruno Adam <dev.adambruno@gmail.com>
2025-11-09 11:01:09 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const config = {
+/*export const config = {
   //domainSource: "https://argologerenciadoraacervos.com.br", // Your WeWeb app preview link
  	domainSource: "https://www.argologerenciadoraacervos.com.br", //?origin=bypass",
   patterns: [
@@ -9,5 +9,21 @@ export const config = {
           //metaDataEndpoint: "https://api.argologerenciadoraacervos.com.br/api:tmCisItK/usercurriculo/{paramuser}/metadata"
       }
       // Add more patterns and their metadata endpoints as needed
+  ]
+};*/
+
+export const config = {
+  // URL base do seu app publicado no WeWeb
+  domainSource: "https://www.argologerenciadoraacervos.com.br",
+
+  patterns: [
+    {
+      // Regex que casa URLs do tipo /perfilpublico/qualquercoisa ou /perfilpublico/qualquercoisa/
+      pattern: "^/perfilpublico/[^/]+/?$",
+
+      // Placeholder do parâmetro DEVE bater exatamente com o nome usado no WeWeb
+      metaDataEndpoint:
+        "https://api.argologerenciadoraacervos.com.br/api:tmCisItK/usercurriculo/{paramCurriculo}/metadata"
+    }
   ]
 };
