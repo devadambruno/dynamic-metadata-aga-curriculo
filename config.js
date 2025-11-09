65d7edafaf60879644586b3fb9b7528f1684e82f
Bruno Adam <dev.adambruno@gmail.com>
2025-11-09 10:21:22 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ export const config = {
  	domainSource: "https://www.argologerenciadoraacervos.com.br?origin=bypass",
   patterns: [
       {
-          pattern: "/perfilpublico/[^/]+",
+          //pattern: "/perfilpublico/[^/]+",
+					pattern: "^/perfilpublico/[^/]+/?$",
           metaDataEndpoint: "https://api.argologerenciadoraacervos.com.br/api:tmCisItK/usercurriculo/{paramuser}/metadata"
       }
       // Add more patterns and their metadata endpoints as needed
